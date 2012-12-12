52e42b49b5193048e9bc15b43a1b1ba783dbc9a7
Jared Sealey <jsealey762@gmail.com>
2012-12-12 13:26:43 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chicoprocess::Application.config.secret_token = 'e31baeba78b8c97cff5b93e030b02e8c7685b65b86e03a97b500d2153183cd0050fb681f17c35c81a2599e35296d7e0c3a34bc335a6296a66e660fbefb87a8ca'
